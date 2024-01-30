00d5a7690f974f19b472fcdb7d370ad454a8e270
adam <itstudy@kakao.com>
2024-01-30 17:06:55 +0900

    4

--- src/AddToDo.jsx
+++ src/AddToDo.jsx
@@ -5,7 +5,37 @@ import {TextField, Paper, Button, Grid} from "@material-ui/core"
 class AddToDo extends React.Component{
     constructor(props){
         super(props);
+
+        //App.js 파일에서 넘겨준 메서드를 add 에 저장
+        this.add = props.add;
+
+        //입력한 문자열을 저장하기 위한 속성을 생성
+        this.state = {item:{title:""}}
     }
+
+    //TextField에서 값을 입력할 때 마다 호출되는 이벤트 리스너
+    onInputChange = (e) => {
+        //state.title에 입력한 내용을 대입
+        const thisItem = this.state.item
+        thisItem.title = e.target.value;
+        this.setState({item:thisItem});
+    }
+
+    //+ 버튼을 눌렀을 때 호출되는 이벤트 리스너
+    onButtonClick = (e) => {
+        //배열에 데이터를 삽입하는 함수를 호출
+        this.add(this.state.item);
+        //텍스트 필드를 초기화
+        this.setState({item:{title:""}})
+    }
+    
+    //Enter를 누르면 호출되는 이벤트 리스너
+    enterKeyEventHandler = (e) => {
+        if(e.key === "Enter"){
+            this.onButtonClick();
+        }
+    }
+
 
     render(){
         return(
--- src/App.js
+++ src/App.js
@@ -17,6 +17,25 @@ class App extends React.Component {
                  }
   }
 
+  //데이터를 추가하는 함수
+  add = (item) => {
+    //react는 props는 수정할 수 없고
+    //state는 수정이 가능하지만 원본을 직접 수정하는 것은 안되고
+    //setState 메서드를 이용해서만 수정이 가능합니다.
+
+    //배열에 데이터를 추가하기 위해서 state의 배열을 복사
+    const thisItems = this.state.items;
+
+    //item의 id 와 done 값을 설정 - title 만 입력하기 때문
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+
+    //복사한 데이터에 데이터를 추가
+    thisItems.push(item)
+    //복사한 데이터를 다시 state에 적용
+    this.setState({items:thisItems});
+  }
+
   render(){
     //배열을 순회하면서 출력할 내용을 생성
     //item은 배열을 순회할 때 각각의 데이터이고 idx는 인덱스
@@ -35,7 +54,7 @@ class App extends React.Component {
     return(
       <div className="App">
         <Container maxWidth="md">
-          <AddToDo />
+          <AddToDo add={this.add}/>
           {display}
         </Container>
       </div>
