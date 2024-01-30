da67da5cd46aa14e85246bf47998780d9632d5f3
adam <itstudy@kakao.com>
2024-01-30 15:45:09 +0900

    second

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React from "react"
 
 import ToDo from "./ToDo";
 
+import {Paper, List} from "@material-ui/core"
+
 class App extends React.Component {
   constructor(props){
     super(props)
@@ -19,9 +21,15 @@ class App extends React.Component {
     //item은 배열을 순회할 때 각각의 데이터이고 idx는 인덱스
     //배열을 순회하면서 출력물을 만들 때는 key를 설정해주어야 합니다.
     //key를 설정하지 않으면 출력에는 문제가 없지만 콘솔에 에러가 출력됩니다.
-    var display = this.state.items.map((item, idx) => (
-      <ToDo item={item} key={item.id} />
-    ));
+    var display = this.state.items.length >0 && (
+      <Paper style={{margin:16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <ToDo item={item} id={idx}/>
+          ))}
+        </List>
+      </Paper>
+    )
 
     return(
       <div className="App">
